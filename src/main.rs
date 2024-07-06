9539212bca8d1ce6aad741bd0cd7d0c8377e79e9
lterry <randoom97@live.com>
2024-07-05 20:02:31 -0600

    wireup stdout & stderr

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
+
     let output = std::process::Command::new(command)
         .args(command_args)
         .output()
@@ -16,7 +17,9 @@ fn main() -> Result<()> {
 
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
+        print!("{}", std_out);
+        let std_err = std::str::from_utf8(&output.stderr)?;
+        eprint!("{}", std_err);
     } else {
         std::process::exit(1);
     }
